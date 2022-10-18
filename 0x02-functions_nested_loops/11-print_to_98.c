ad30a749745fc3793c823c473b628e7d04d1dd6e
melodytangus <melodytangus@gmail.com>
2022-10-18 08:38:22 -0700

    0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,25 +13,14 @@ if (n > 98)
 for (; n > 98; n--)
 {
 printf("%d, ", n);
-	  
-	}
-      
-    }
-  
-  else if (n < 98)
-    
-    {
-      
-      for (; n < 98; n++)
-	
-	{
-	  
-	  printf("%d, ", n);
-	  
-	}
-      
-    }
-  
-  printf("%d\n", n);
-  
+}
+}
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
+printf("%d\n", n);
 }
