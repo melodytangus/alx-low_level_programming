d34f12ab58b80f37f816538db6854090a4e88279
melodytangus <melodytangus@gmail.com>
2022-10-18 08:45:19 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+long int sum;
+long int n1 = 0;
+long int n2 = 1;
+for (i = 1; i <= 50; i++)
+{
+sum = n1 + n2;
+printf("%ld", sum);
+n1 = n2;
+n2 = sum;
+if (i < 50)
+printf(", ");
+}
+printf("\n");
+return (0);
+}
