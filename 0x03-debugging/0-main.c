37ecde5f26ffbc51dc8cc3d56801b1af05a67ea3
melodytangus <melodytangus@gmail.com>
2022-10-19 23:49:45 -0700

    testing function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - calls the positive or negative function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
