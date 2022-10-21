8b1ebfd9967e879acfbde2a2b91f9156b275162a
melodytangus <melodytangus@gmail.com>
2022-10-21 10:18:19 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Print large number character at a time
+ * @n: Number to print
+ * Return: Void
+ */
+
+void print_number(int n)
+{
+if (n < 0)
+_putchar('-');
+n *= -1;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
+}
