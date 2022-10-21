207ca25c7a04bf246b68001ba5698d83574b55ca
melodytangus <melodytangus@gmail.com>
2022-10-21 09:20:13 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Fizz Buzz
+ * Return: void
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i < 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+
+else if (i % 5 == 0)
+printf("Buzz ");
+
+else if (i % 3 == 0)
+printf("Fizz ");
+
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
+}
