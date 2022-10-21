e6d7052aeb0ce2d486fe1e635c3bfad1d3e22985
melodytangus <melodytangus@gmail.com>
2022-10-21 12:12:25 -0700

    correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,27 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+i = 1;
+printf("%d", i);
+for (i = 2; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz ");
-
-else if (i % 5 == 0)
-printf("Buzz ");
-
+}
 else if (i % 3 == 0)
+{
 printf("Fizz ");
-
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
 else
+{
 printf("%d ", i);
 }
-printf("Buzz\n");
+}
+printf("\n");
 return (0);
 }
