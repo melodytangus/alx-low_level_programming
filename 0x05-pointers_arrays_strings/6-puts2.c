157aa6b06f26fe2c71248e80967e91382cea46bd
melodytangus <melodytangus@gmail.com>
2022-10-25 07:47:19 -0700

    print each character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - print each character
+ * @str: Pointer to string location
+ *Return: Null/Void
+ */
+void puts2(char *str)
+{
+int len = 0;
+char *y = str;
+int z;
+while (*y != '\0')
+{
+y++;
+len++;
+}
+len--;
+for (z = 0 ; z <= len ; z++)
+{
+if (z % 2 == 0)
+{
+putchar(str[z]);
+}
+}
+putchar('\n');
+}
