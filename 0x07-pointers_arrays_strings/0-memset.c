971579843fb240a46a9e3fb558443811c486043d
melodytangus <melodytangus@gmail.com>
2022-10-31 23:32:17 -0700

    set memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills n bytes of the memory pointed by s with the byte b
+ * @s: Pointer to array location
+ * @b: Constant byte to replace stored date
+ * @n: number of bytes to be replaced
+ * Return: Pointer to memory s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
