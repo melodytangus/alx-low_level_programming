3f1cba12252cc2e7b2efd502116a3117ad40832d
melodytangus <melodytangus@gmail.com>
2022-11-01 22:36:15 -0700

    length corrected

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 int i = 0;
-int j=0;
+int j;
 int len = 0;
 
 while (s[i])
 {
-j=0;
+j = 0;
 while (accept[j])
 {
-if (s[i] == accept[j])
+if (s[i] == '\0')
+{
+break;
+}
+if (s[i] == accept[j] && s[i] != '\0')
 {
 len++;
 break;
