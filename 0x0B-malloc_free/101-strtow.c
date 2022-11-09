fa4ca3a8dc4bd0051ccc8d041dd418cf33f7c50e
melodytangus <melodytangus@gmail.com>
2022-11-09 08:33:12 -0800

    split string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,54 +1,102 @@
 #include "main.h"
-/**
- * strtow - Concatenate all arguements
- * @str: String Input to split
- * Return: Pointer to new string
- **/
-char **strtow(char *str)
-{
-	char **stray;
-	int i = 0;
-	int j = 0;
-	int column = 0;
-	int len = 0;
-	int ch = 0;
-	int row[10];
+#include <stdlib.h>
 
-	while (str[len++])
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
 	{
-		if (str[len - 1] == ' ' && str[len] != ' ')
-			column++;
+		len++;
+		index++;
 	}
-	column++;;
-	stray = malloc(sizeof(char) * column);
-	if (stray == NULL)
+
+	return (len);
+}
+
+/**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
 	{
-		free(stray);
-		return(NULL);
-	}
-	len = 0; 
-	i = 0;
-	while (str[len++])
-	{
-		if (str[len - 1] != ' ' && str[len] != ' ')
-			ch++;
-		if (str[len - 1] == ' ' && str[len] != ' ')
+		if (*(str + index) != ' ')
 		{
-			row[i] = ch;
-			stray[i] = malloc(sizeof(char) * row[i] + 1);
-			ch = 0;
-			i++;
+			words++;
+			index += word_len(str + index);
 		}
 	}
-	len = 0;
-	while (str[len])
+
+	return (words);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
 	{
-		if (str[len] == ' ' && str[len + 1] != ' ')
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
 		{
-			j = 0;
-			for (i = 0; i < row[i]; i++)
-				stray[i][j++] = str[len++];
-		}	
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
 	}
-	return (stray);
+	strings[w] = NULL;
+
+	return (strings);
 }
