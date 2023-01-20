0d2e84ea2518ae64891c7c2a89b56fbf99b89471
melodytangus <melodytangus@gmail.com>
2023-01-20 18:07:52 +0300

    create dynamic lib

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -6,5 +6,25 @@
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
+{char *h = haystack;
+ char *n = needle;
+
+while (*h)
 {
+n = needle;
+h = haystack;
+while (*n)
+{
+if (*h == *n)
+{
+n++;
+h++;																								
+}else
+break;
+}
+if (*n == '\0')
+return (haystack);
+haystack++;
+}
+return (0);
 }
