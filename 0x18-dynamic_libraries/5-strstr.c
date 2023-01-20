b4c8c4b47713b24c4174a6fa57d4a3b5c8ffd1af
melodytangus <melodytangus@gmail.com>
2023-01-20 17:31:05 +0300

    dynamic lib

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,10 @@
+/**
+ * _strstr - locate a substring
+ * @haystack: string to look in
+ * @needle: string to locate
+ * Return: A pointer to the beginning of the substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
